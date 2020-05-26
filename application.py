567040b0f0a11c377e09d2011b115b3564d3c102
Kevin Hsu <kevin.hsu@berkeley.edu>
2020-05-25 22:25:52 -0700

    session variables to replace globals

--- application.py
+++ application.py
@@ -16,16 +16,6 @@ from search_for_venues import nearby_venues, get_details, distance_weighted_orde
 from utils import Venue, venues_to_dicts, dicts_to_venues, miles_to_meters
 
 """
-Global Variables
-"""
-location_data = {}
-venues = []
-original_location = ()
-query = ""
-suggested_index = 0
-radius = 0
-
-"""
 Constants for Errors 
 """
 API_REQUEST = 0
@@ -48,12 +38,12 @@ class PrevVenue(FlaskForm):
 @app.route("/home")
 @app.route("/", methods = ["POST", "GET"])
 def home():
-    global location_data
-    global venues
-    global original_location
-    global query
-    global suggested_index
-    global radius
+    # global location_data
+    # global venues
+    # global original_location
+    # global query
+    # global suggested_index
+    # global radius
         
     # Instantiation of forms 
     form = TakeQuery()
@@ -61,51 +51,51 @@ def home():
     prev_venue = PrevVenue()
     
     # Repeatedly attempt to get IP address until successful 
-    location_data = {}
-    while "timestamp" not in location_data.keys()\
-        or "latitude" not in location_data.keys()\
-        or "longitude" not in location_data.keys()\
-        or "city" not in location_data.keys():
-        location_data = get_location_data()
-    original_location = (location_data["latitude"], location_data["longitude"])
+    session["location_data"] = {}
+    while "timestamp" not in session["location_data"].keys()\
+        or "latitude" not in session["location_data"].keys()\
+        or "longitude" not in session["location_data"].keys()\
+        or "city" not in session["location_data"].keys():
+        session["location_data"] = get_location_data()
+    session["original_location"] = (session["location_data"]["latitude"], session["location_data"]["longitude"])
     
     if form.validate_on_submit():
         # Case where main button is clicked 
-        venues = []
+        session["venues"] = []
 
         # No venues observed yet, get list of venues
-        while len(venues) == 0:
-            query = form.query.data
-            radius = miles_to_meters(form.radius.data)
-            venues = nearby_venues(location_data, query, radius)
-            if venues == "API Usage Exceeded":
+        while len(session["venues"]) == 0:
+            session["query"] = form.query.data
+            session["radius"] = miles_to_meters(form.radius.data)
+            session["venues"] = nearby_venues(session["location_data"], session["query"], session["radius"])
+            if session["venues"] == "API Usage Exceeded":
                 # Case that API does not allow new requests, nothing to do 
                 return render_template("home.html", form = form, error_status = API_REQUEST)
-            if venues == []:
+            if session["venues"] == []:
                 # Case that there are no venues found 
                 return render_template("home.html", form = form, error_status = NO_VENUES)
-            venues = distance_weighted_order(venues, original_location)
+            session["venues"] = distance_weighted_order(session["venues"], session["original_location"])
 
         # Successfully acquired list of venues at this point 
-        suggested_index = 0
-        suggested = venues[suggested_index]
+        session["suggested_index"] = 0
+        suggested = session["venues"][session["suggested_index"]]
         if suggested.details == None:
             if get_details(suggested) == "API Usage Exceeded":
                 # Case that API does not allow new requests, nothing to do 
                 return render_template("home.html", form = form, error_status = API_REQUEST)
         suggested.assign_members()
-        if len(venues) == 1:
+        if len(session["venues"]) == 1:
             return render_template("home.html", form = form, suggested = suggested)
         return render_template("home.html", form = form, next_venue = next_venue, suggested = suggested)
 
     if prev_venue.prev_query.data and prev_venue.validate():
         # Case where 'prev venue' button is clicked
-        if suggested_index <= 1:
+        if session["suggested_index"] <= 1:
             # Case where there are no prev venues following this query
-            suggested_index = max(suggested_index - 1, 0)
-            return render_template("home.html", form = form, next_venue = next_venue, suggested = venues[suggested_index])
-        suggested_index -= 1
-        suggested = venues[suggested_index]
+            session["suggested_index"] = max(session["suggested_index"] - 1, 0)
+            return render_template("home.html", form = form, next_venue = next_venue, suggested = session["venues"][session["suggested_index"]])
+        session["suggested_index"] -= 1
+        suggested = session["venues"][session["suggested_index"]]
         if suggested.details == None:
             if get_details(suggested) == "API Usage Exceeded":
                 # Case that API does not allow new requests, nothing to do 
@@ -114,12 +104,12 @@ def home():
 
     if next_venue.next_query.data and next_venue.validate():
         # Case where 'next venue' button is clicked
-        if suggested_index >= len(venues) - 2:
+        if session["suggested_index"] >= len(session["venues"]) - 2:
             # Case where there are no next venues following this query
-            suggested_index = min(suggested_index + 1, len(venues) - 1)
-            return render_template("home.html", form = form, prev_venue = prev_venue, suggested = venues[suggested_index])
-        suggested_index += 1
-        suggested = venues[suggested_index]
+            session["suggested_index"] = min(session["suggested_index"] + 1, len(session["venues"]) - 1)
+            return render_template("home.html", form = form, prev_venue = prev_venue, suggested = session["venues"][session["suggested_index"]])
+        session["suggested_index"] += 1
+        suggested = session["venues"][session["suggested_index"]]
         if suggested.details == None:
             if get_details(suggested) == "API Usage Exceeded":
                 # Case that API does not allow new requests, nothing to do 
